01590a5ced745a0ce08a09f2f4056bf9da9604d4
Vladislav <baiborodov333@mail.ru>
2025-01-20 13:30:23 +0500

    book-cover-set

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+"use client"
+import Image from 'next/image';
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const Header = () => {
+  const pathname = usePathname();
+
+  return (
+    <header className="my-10 flex justify-between gap-5 text-white">
+      <Link href="/">
+	<Image src="/icons/logo.svg" width={40} height={40} alt='logo'/>
+	</Link>
+
+      <ul className="flex flex-row items-center gap-8">
+        <li>
+          <Link
+            href="/library"
+            className={cn(
+              "text-base cursor-pointer capitalize",
+              pathname === "/library" ? "text-light-200" : "text-light-100"
+            )}
+          >
+            Library
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
